bdac5586c3b9cffe4e64b96b0953a2863c86bc29
thinhle_ctr <thinhle_ctr@fortna.com>
2026-03-28 11:27:11 +0700

    update

--- sia-booking/src/main/java/com/sia/booking/repository/BookingRepository.java
+++ sia-booking/src/main/java/com/sia/booking/repository/BookingRepository.java
@@ -3,7 +3,5 @@ package com.sia.booking.repository;
 import com.sia.booking.model.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
